e7e120e407c6ac5c3b6658446c880463907870cd
ZvonimirSun <hi@iszy.xyz>
2026-05-07 14:27:48 +0800

    feat: 初始化项目

---
+++ content.config.ts
@@ -0,0 +1,3 @@
+import contentConfig from '@zvonimirsun/nuxt-theme-next/content.config'
+
+export default contentConfig
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@zvonimirsun/nuxt-theme-next'],
+  modules: ['@nuxt/eslint'],
+  runtimeConfig: {
+    public: {
+      title: '随遇而安',
+      subtitle: '在自由的空气中漂浮',
+      description: '生活吐槽 & 学习记录'
+    }
+  },
+  devtools: { enabled: true },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        'dayjs', // CJS
+      ],
+    }
+  }
+})
